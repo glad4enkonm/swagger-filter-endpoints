f44e33ff412953bc1a34d96b4f78abf2c2d0e0e2
no nono <some@email.com>
2023-12-08 00:10:12 +0300

    init

---
+++ README.md
@@ -0,0 +1,25 @@
+# Swagger File Filter
+
+This Python script filters a Swagger file (either JSON or YAML) to only include specified endpoints and their associated definitions.
+
+## Description
+
+The script reads a Swagger file, filters out the specified endpoints, collects all the definitions used by these endpoints, and writes the filtered Swagger data to a new YAML file.
+
+## How to Start
+
+1. Install the required Python libraries if you haven't already:
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+2. Run the Python script and enter the full path for your Swagger file when prompted. If no path is entered, the script will look for a file named 'swagger.json' in the same directory:
+    ```bash
+    python script.py --swagger_file path_to_your_swagger_file --endpoints_to_keep /endpoint1 /endpoint2 /endpoint3
+    ```
+
+3. The filtered Swagger data will be saved to a new YAML file named 'filtered_swagger.yaml' in the same directory.
+
+## Note
+
+The script currently only supports Swagger files in JSON and YAML formats. Other formats are not supported.
---
+++ main.py
@@ -0,0 +1,57 @@
+import json
+import yaml
+import os
+import argparse
+
+# Function to get all the definitions referenced in a given operation
+def get_definitions_from_operation(operation):
+    definitions = set()
+    for response in operation.get('responses', {}).values():
+        schema = response.get('schema', {})
+        ref = schema.get('$ref')
+        if ref and ref.startswith('#/definitions/'):
+            definitions.add(ref.split('/')[-1])
+    return definitions
+
+def filter_swagger(swagger_file, endpoints_to_keep):
+    file_extension = os.path.splitext(swagger_file)[1]
+
+    # Load the original Swagger file
+    with open(swagger_file, 'r') as file:
+        if file_extension == '.json':
+            swagger_data = json.load(file)
+        elif file_extension == '.yaml' or file_extension == '.yml':
+            swagger_data = yaml.safe_load(file)
+        else:
+            print("Unsupported file type")
+
+    # Filter the paths and collect definitions that are used
+    filtered_paths = {}
+    used_definitions = set()
+    for path, info in swagger_data['paths'].items():
+        if path in endpoints_to_keep:
+            filtered_paths[path] = info
+            for method in info.values():
+                used_definitions.update(get_definitions_from_operation(method))
+
+    # Filter the definitions
+    filtered_definitions = {def_name: def_info for def_name, def_info in swagger_data['definitions'].items() if def_name in used_definitions}
+
+    # Replace the paths and definitions in the original Swagger data
+    swagger_data['paths'] = filtered_paths
+    swagger_data['definitions'] = filtered_definitions
+
+    # Save the filtered Swagger JSON to a new file
+    with open('filtered_swagger.yaml', 'w') as file:
+        yaml.dump(swagger_data, file, default_flow_style=False)
+
+def main():
+    parser = argparse.ArgumentParser(description='Filter a Swagger file to only include certain endpoints.')
+    parser.add_argument('--swagger_file', type=str, default='swagger.json', help='The full path for a swagger.json (or yaml) file')
+    parser.add_argument('--endpoints_to_keep', nargs='+', default=[], help='The endpoints you want to keep')
+    args = parser.parse_args()
+
+    filter_swagger(args.swagger_file, args.endpoints_to_keep)
+
+if __name__ == "__main__":
+    main()
